057600ec3dc871ab8f71013383476956897d6853
typicalguy6363-ops <typicalguy6363@gmail.com>
2026-07-16 09:00:48 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// PersianAcademy JavaScript
+
+const startButton = document.querySelector(".start");
+
+if (startButton) {
+    startButton.addEventListener("click", () => {
+        alert("Welcome to PersianAcademy! 🚀");
+    });
+}
+
+const signInButton = document.querySelector(".login");
+
+if (signInButton) {
+    signInButton.addEventListener("click", () => {
+        alert("Sign In page coming soon!");
+    });
+}
+
+const signUpButton = document.querySelector(".signup");
+
+if (signUpButton) {
+    signUpButton.addEventListener("click", () => {
+        alert("Sign Up page coming soon!");
+    });
+}
